7af2cc9bdd3c961107034326f5beece27935564b
JTSage.com Root User <root@jtsage.com>
2009-06-27 15:40:36 -0400

    Backend DB abstraction layer

--- lib/user.php
+++ lib/user.php
@@ -37,6 +37,251 @@ function user_getarmor ( $userid ) {
         return $line['armor'];
 }
 
+function user_getweapon ( $userid ) {
+        global $db, $MYSQL_PREFIX;
+        $sql = "SELECT weapon FROM {$MYSQL_PREFIX}stats WHERE userid = {$userid}";
+        $result = mysql_query($sql, $db);
+        $line = mysql_fetch_array($result);
+        return $line['weapon'];
+}
+
+function user_getgold ( $userid ) {
+        global $db, $MYSQL_PREFIX;
+        $sql = "SELECT gold FROM {$MYSQL_PREFIX}stats WHERE userid = {$userid}";
+        $result = mysql_query($sql, $db);
+        $line = mysql_fetch_array($result);
+        return $line['gold'];
+}
+
+function user_getdef ( $userid ) {
+        global $db, $MYSQL_PREFIX;
+        $sql = "SELECT def FROM {$MYSQL_PREFIX}stats WHERE userid = {$userid}";
+        $result = mysql_query($sql, $db);
+        $line = mysql_fetch_array($result);
+        return $line['def'];
+}
+
+function user_getstr ( $userid ) {
+        global $db, $MYSQL_PREFIX;
+        $sql = "SELECT str FROM {$MYSQL_PREFIX}stats WHERE userid = {$userid}";
+        $result = mysql_query($sql, $db);
+        $line = mysql_fetch_array($result);
+        return $line['str'];
+}
+
+function user_gethp ( $userid ) {
+        global $db, $MYSQL_PREFIX;
+        $sql = "SELECT hp FROM {$MYSQL_PREFIX}stats WHERE userid = {$userid}";
+        $result = mysql_query($sql, $db);
+        $line = mysql_fetch_array($result);
+        return $line['hp'];
+}
+
+function user_gethpmax ( $userid ) {
+        global $db, $MYSQL_PREFIX;
+        $sql = "SELECT hpmax FROM {$MYSQL_PREFIX}stats WHERE userid = {$userid}";
+        $result = mysql_query($sql, $db);
+        $line = mysql_fetch_array($result);
+        return $line['hpmax'];
+}
+
+function user_getexp ( $userid ) {
+        global $db, $MYSQL_PREFIX;
+        $sql = "SELECT exp FROM {$MYSQL_PREFIX}stats WHERE userid = {$userid}";
+        $result = mysql_query($sql, $db);
+        $line = mysql_fetch_array($result);
+        return $line['exp'];
+}
+
+function user_getgems ( $userid ) {
+        global $db, $MYSQL_PREFIX;
+        $sql = "SELECT gems FROM {$MYSQL_PREFIX}stats WHERE userid = {$userid}";
+        $result = mysql_query($sql, $db);
+        $line = mysql_fetch_array($result);
+        return $line['gems'];
+}
+
+function user_getcharm ( $userid ) {
+        global $db, $MYSQL_PREFIX;
+        $sql = "SELECT charm FROM {$MYSQL_PREFIX}stats WHERE userid = {$userid}";
+        $result = mysql_query($sql, $db);
+        $line = mysql_fetch_array($result);
+        return $line['charm'];
+}
+
+function user_getffight ( $userid ) {
+        global $db, $MYSQL_PREFIX;
+        $sql = "SELECT ffight FROM {$MYSQL_PREFIX}stats WHERE userid = {$userid}";
+        $result = mysql_query($sql, $db);
+        $line = mysql_fetch_array($result);
+        return $line['ffight'];
+}
+
+function user_getpfight ( $userid ) {
+        global $db, $MYSQL_PREFIX;
+        $sql = "SELECT pfight FROM {$MYSQL_PREFIX}stats WHERE userid = {$userid}";
+        $result = mysql_query($sql, $db);
+        $line = mysql_fetch_array($result);
+        return $line['pfight'];
+}
+
+function user_getskilluse ( $userid ) {
+        global $db, $MYSQL_PREFIX;
+        $sql = "SELECT skilluse FROM {$MYSQL_PREFIX}stats WHERE userid = {$userid}";
+        $result = mysql_query($sql, $db);
+        $line = mysql_fetch_array($result);
+        return $line['skilluse'];
+}
+
+function user_setarmor ( $userid, $ins ) {
+        global $db, $MYSQL_PREFIX;
+        $sql = "UPDATE {$MYSQL_PREFIX}stats SET armor = {$ins} WHERE userid = {$userid}";
+        $result = mysql_query($sql, $db);
+}
+
+function user_setweapon ( $userid, $ins ) {
+        global $db, $MYSQL_PREFIX; 
+        $sql = "UPDATE {$MYSQL_PREFIX}stats SET weapon = {$ins} WHERE userid = {$userid}";
+        $result = mysql_query($sql, $db);
+}
+
+function user_setlevel ( $userid, $ins ) {
+        global $db, $MYSQL_PREFIX;
+        $sql = "UPDATE {$MYSQL_PREFIX}stats SET level = {$ins} WHERE userid = {$userid}";
+        $result = mysql_query($sql, $db);
+}
+
+function user_givegold ( $userid, $ins ) {
+        global $db, $MYSQL_PREFIX;
+        $sql = "UPDATE {$MYSQL_PREFIX}stats SET gold = ( gold + {$ins} ) WHERE userid = {$userid}";
+        $result = mysql_query($sql, $db);
+}
+
+function user_takegold ( $userid, $ins ) {
+        global $db, $MYSQL_PREFIX;
+        $sql = "UPDATE {$MYSQL_PREFIX}stats SET gold = ( gold - {$ins} ) WHERE userid = {$userid}";
+        $result = mysql_query($sql, $db);
+}
+
+function user_givedef ( $userid, $ins ) {
+        global $db, $MYSQL_PREFIX;
+        $sql = "UPDATE {$MYSQL_PREFIX}stats SET def = ( def + {$ins} ) WHERE userid = {$userid}";
+        $result = mysql_query($sql, $db);
+}
+
+function user_takedef ( $userid, $ins ) {
+        global $db, $MYSQL_PREFIX;
+        $sql = "UPDATE {$MYSQL_PREFIX}stats SET def = ( def - {$ins} ) WHERE userid = {$userid}";
+        $result = mysql_query($sql, $db);
+}
+
+function user_givestr ( $userid, $ins ) {
+        global $db, $MYSQL_PREFIX;
+        $sql = "UPDATE {$MYSQL_PREFIX}stats SET str = ( str + {$ins} ) WHERE userid = {$userid}";
+        $result = mysql_query($sql, $db);
+}
+
+function user_takestr ( $userid, $ins ) {
+        global $db, $MYSQL_PREFIX;
+        $sql = "UPDATE {$MYSQL_PREFIX}stats SET str = ( str - {$ins} ) WHERE userid = {$userid}";
+        $result = mysql_query($sql, $db);
+}
+
+function user_giveexp ( $userid, $ins ) {
+        global $db, $MYSQL_PREFIX;
+        $sql = "UPDATE {$MYSQL_PREFIX}stats SET exp = ( exp + {$ins} ) WHERE userid = {$userid}";
+        $result = mysql_query($sql, $db);
+}
+
+function user_takeexp ( $userid, $ins ) {
+        global $db, $MYSQL_PREFIX;
+        $sql = "UPDATE {$MYSQL_PREFIX}stats SET exp = ( exp - {$ins} ) WHERE userid = {$userid}";
+        $result = mysql_query($sql, $db);
+}
+
+function user_givehp ( $userid, $ins ) {
+        global $db, $MYSQL_PREFIX;
+        $sql = "UPDATE {$MYSQL_PREFIX}stats SET hp = ( hp + {$ins} ) WHERE userid = {$userid}";
+        $result = mysql_query($sql, $db);
+}
+
+function user_takehp ( $userid, $ins ) {
+        global $db, $MYSQL_PREFIX;
+        $sql = "UPDATE {$MYSQL_PREFIX}stats SET hp = ( hp - {$ins} ) WHERE userid = {$userid}";
+        $result = mysql_query($sql, $db);
+}
+
+function user_givehpmax ( $userid, $ins ) {
+        global $db, $MYSQL_PREFIX;
+        $sql = "UPDATE {$MYSQL_PREFIX}stats SET hpmax = ( hpmax + {$ins} ) WHERE userid = {$userid}";
+        $result = mysql_query($sql, $db);
+}
+
+function user_takehpmax ( $userid, $ins ) {
+        global $db, $MYSQL_PREFIX;
+        $sql = "UPDATE {$MYSQL_PREFIX}stats SET hpmax = ( hpmax - {$ins} ) WHERE userid = {$userid}";
+        $result = mysql_query($sql, $db);
+}
+
+function user_givegems ( $userid, $ins ) {
+        global $db, $MYSQL_PREFIX;
+        $sql = "UPDATE {$MYSQL_PREFIX}stats SET gems = ( gems + {$ins} ) WHERE userid = {$userid}";
+        $result = mysql_query($sql, $db);
+}
+
+function user_takegems ( $userid, $ins ) {
+        global $db, $MYSQL_PREFIX;
+        $sql = "UPDATE {$MYSQL_PREFIX}stats SET gems = ( gems - {$ins} ) WHERE userid = {$userid}";
+        $result = mysql_query($sql, $db);
+}
+
+function user_givecharm ( $userid, $ins ) {
+        global $db, $MYSQL_PREFIX;
+        $sql = "UPDATE {$MYSQL_PREFIX}stats SET charm = ( charm + {$ins} ) WHERE userid = {$userid}";
+        $result = mysql_query($sql, $db);
+}
+
+function user_takecharm ( $userid, $ins ) {
+        global $db, $MYSQL_PREFIX;
+        $sql = "UPDATE {$MYSQL_PREFIX}stats SET charm = ( charm - {$ins} ) WHERE userid = {$userid}";
+        $result = mysql_query($sql, $db);
+}
+
+function user_giveffight ( $userid, $ins ) {
+        global $db, $MYSQL_PREFIX;
+        $sql = "UPDATE {$MYSQL_PREFIX}stats SET ffight = ( ffight + {$ins} ) WHERE userid = {$userid}";
+        $result = mysql_query($sql, $db);
+}
+
+function user_takeffight ( $userid, $ins ) {
+        global $db, $MYSQL_PREFIX;
+        $sql = "UPDATE {$MYSQL_PREFIX}stats SET ffight = ( ffight - {$ins} ) WHERE userid = {$userid}";
+        $result = mysql_query($sql, $db);
+}
+
+function user_givepfight ( $userid, $ins ) {
+        global $db, $MYSQL_PREFIX;
+        $sql = "UPDATE {$MYSQL_PREFIX}stats SET pfight = ( pfight + {$ins} ) WHERE userid = {$userid}";
+        $result = mysql_query($sql, $db);
+}
+
+function user_takepfight ( $userid, $ins ) {
+        global $db, $MYSQL_PREFIX;
+        $sql = "UPDATE {$MYSQL_PREFIX}stats SET pfight = ( pfight - {$ins} ) WHERE userid = {$userid}";
+        $result = mysql_query($sql, $db);
+}
+
+function user_giveskilluse ( $userid, $ins ) {
+        global $db, $MYSQL_PREFIX;
+        $sql = "UPDATE {$MYSQL_PREFIX}stats SET skilluse = ( skilluse + {$ins} ) WHERE userid = {$userid}";
+        $result = mysql_query($sql, $db);
+}
+
+function user_takeskilluse ( $userid, $ins ) {
+        global $db, $MYSQL_PREFIX;
+        $sql = "UPDATE {$MYSQL_PREFIX}stats SET skilluse = ( skilluse - {$ins} ) WHERE userid = {$userid}";
+        $result = mysql_query($sql, $db);
+}
 
 function user_chkpass ($userid, $pass) {
 	global $db, $MYSQL_PREFIX;
