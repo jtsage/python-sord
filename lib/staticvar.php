d29acefa44864c98381efbf0813394e3f99722a2
JTSage.com Root User <root@jtsage.com>
2009-06-27 15:40:58 -0400

    Armor and Weapon prices / strength / defense / prerequisites

--- lib/staticvar.php
+++ lib/staticvar.php
@@ -5,23 +5,30 @@ $classes = array(
 	2 => "Mystical",
 	3 => "Thief" );
 
-$weapon[1] = "Stick";
-$weapon[2] = "Dagger";
-$weapon[3] = "Short Sword";
-$weapon[4] = "Long Sword";
-$weapon[5] = "Huge Axe";
-$weapon[6] = "Bone Cruncer";
-$weapon[7] = "Twin Swords";
-$weapon[8] = "Power Axe";
-$weapon[9] = "Able's Sword";
-$weapon[10] = "Wan's Weapon";
-$weapon[11] = "Spear Of Gold";
-$weapon[12] = "Crystal Shard";
-$weapon[13] = "Niras's Teeth";
-$weapon[14] = "Blood Sword";
-$weapon[15] = "Death Sword";
+$weapon = array( 
+	0 => "None",
+	1 => "Stick",
+	2 => "Dagger",
+	3 => "Short Sword",
+	4 => "Long Sword",
+	5 => "Huge Axe",
+	6 => "Bone Cruncer",
+	7 => "Twin Swords",
+	8 => "Power Axe",
+	9 => "Able's Sword",
+	10 => "Wan's Weapon",
+	11 => "Spear Of Gold",
+	12 => "Crystal Shard",
+	13 => "Niras's Teeth",
+	14 => "Blood Sword",
+	15 => "Death Sword");
+
+$weaponprice = array ( 0, 200, 1000, 3000, 10000, 30000, 100000, 150000, 200000, 400000, 1000000, 4000000, 10000000, 40000000, 100000000, 400000000 );
+$weaponstr   = array ( 0, 5, 10, 20, 30, 40, 60, 80, 120, 180, 250, 350, 500, 800, 1200, 1800 );
+$weaponnstr  = array ( 0, 0, 0, 15, 22, 32, 44, 64, 99, 149, 224, 334, 334, 334, 334, 334 );
 
 $armor = array(
+	0 => "None",
 	1 => "Coat",
 	2 => "Heavy Coat",
 	3 => "Leather Vest",
@@ -37,4 +44,8 @@ $armor = array(
 	13 => "Belar's Mail",
 	14 => "Golden Armour",
 	15 => "Armour Of Lore" );
+
+$armorprice = array ( 0, 200, 1000, 3000, 10000, 30000, 100000, 150000, 200000, 400000, 1000000, 4000000, 10000000, 40000000, 100000000, 400000000 );
+$armordef   = array ( 0, 1, 3, 10, 15, 25, 35, 50, 75, 100, 150, 225, 300, 400, 600, 1000 );
+$armorndef  = array ( 0, 0, 0, 2, 5, 10, 20, 35, 57, 92, 152, 232, 232, 232, 232, 232 );
 ?>
